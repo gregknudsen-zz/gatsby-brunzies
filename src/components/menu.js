5dda30a9ba59b6a988b4e7fbf8f5a9f7c16cad53
Greg Knudsen <gknudsen@duqlight.com>
2019-10-09 11:33:00 -0400

    start to mess with pdf plugin

---
+++ src/components/menu.js
@@ -0,0 +1,20 @@
+// import React from "react";
+// import { useStaticQuery, graphql } from "gatsby";
+
+// const Menu = () => {
+//   const data = useStaticQuery(graphql `
+//     query {
+//       allPdf {
+//         edges {
+//           node {
+//             content
+//           }
+//         }
+//       }
+//     }
+//   `)
+
+//   return <Menu pdf={data.allPdf.edges[0].node.content}/>
+// }
+
+// export default Menu;
