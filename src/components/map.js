278ce1cdbbf21d093459aff3fb4d70cd9d6326a3
Greg Knudsen <gknudsen@duqlight.com>
2019-10-21 08:10:07 -0400

    change master to reflect map usage

--- src/components/map.js
+++ src/components/map.js
@@ -1,13 +1,37 @@
 import React from "react"
 import UseMaps from "../hooks/use-maps"
+import GoogleMapReact from "google-map-react"
 
 const Map = () => {
   const useMaps = UseMaps();
+  const defaultProps = {
+    center: {
+      lat: useMaps.location.lat,
+      lng: useMaps.location.lng
+    },
+    zoom: 15
+  };
+
   console.log('USE MAPS', useMaps)
+  // const AnyReactComponent = () => <div><i className="fas fa-map-pin fa-2x"></i></div>;
+
   return (
     <>
-      {/* {useMaps.location.lat}<br /> */}
-      {/* {useMaps.location.lng} */}
+    <div className="map-container" style={{ height: '22rem', width: '100%' }}>
+        <GoogleMapReact
+          bootstrapURLKeys={{ key: process.env.GATSBY_YOUR_GOOGLE_MAPS_GEOCODING_API_KEY }}
+          defaultCenter={defaultProps.center}
+          defaultZoom={defaultProps.zoom}
+        >
+          {/* <AnyReactComponent
+            lat={useMaps.location.lat}
+            lng={useMaps.location.lng}
+
+          /> */}
+        </GoogleMapReact>
+      </div>
+      {/* {useMaps.location.lat}<br />
+      {useMaps.location.lng} */}
     </>
 
   )
